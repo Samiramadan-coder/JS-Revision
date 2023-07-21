0c25f995bb4aab4b6be1640229826e357be38081
sami <samyramadan.bts@gmail.com>
2023-07-21 22:53:13 +0300

    introduction

---
+++ playground.js
@@ -0,0 +1,18 @@
+// variables are placeholders to store information and data in you app
+// the variable refer to the value store in it.
+// operators are used to assign values, compare values, perform arithmatic operations.
+
+
+// name = "Sami Ramadan";
+// console.log(name);
+
+// food = Number(prompt("How much was the food?"));
+// console.log(food);
+// tipPercentage = Number(prompt("tip %?")) / 100;
+// tipAmount = food * tipPercentage;
+// total = food + tipAmount;
+
+// console.log('tip', tipAmount);
+// console.log('total', total);
+// alert(tipAmount);
+
